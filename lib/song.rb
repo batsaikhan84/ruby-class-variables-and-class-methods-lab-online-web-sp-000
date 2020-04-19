59c035a0e9dbcbfd51295b0332d28f15d2753e8c
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-19 02:49:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,8 +9,8 @@ class Song
   
   def initialize(name, artist, genre)
     @name = name
-    @@artists = artist
-    @@genres = genre
+    @@artists << artist
+    @@genres << genre
     @@count += 1
   end
   
